bf5062ca9945f026fbc7c1de6a26b1942f2311cf
nishen <nish.naidoo@gmail.com>
2017-10-01 13:09:45 +1100

    Added export clause.

--- src/scripts/resource-sharing-partners-sync.sh
+++ src/scripts/resource-sharing-partners-sync.sh
@@ -2,13 +2,13 @@
 
 APP=${project.artifactId}-${project.version}.jar
 
-ALMA_URL=${alma.url}
-ELASTIC_URL=${elastic.url}
-ELASTIC_USR=${elastic.usr}
-ELASTIC_PWD=${elastic.pwd}
-SYNC_HOST=${sync.host}
-SYNC_PORT=${sync.port}
-SYNC_PATH=${sync.path}
+export ALMA_URL=${alma.url}
+export ELASTIC_URL=${elastic.url}
+export ELASTIC_USR=${elastic.usr}
+export ELASTIC_PWD=${elastic.pwd}
+export SYNC_HOST=${sync.host}
+export SYNC_PORT=${sync.port}
+export SYNC_PATH=${sync.path}
 
 JAVA_OPTS="-Xms1g -Xmx1g"
 
