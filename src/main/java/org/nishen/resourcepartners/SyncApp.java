bf85dd935cc601ffb158c0b267c0f0f090f696bc
nishen <nishen.naidoo@mq.edu.au>
2018-02-20 22:35:55 +1100

    partial

--- src/main/java/org/nishen/resourcepartners/SyncApp.java
+++ src/main/java/org/nishen/resourcepartners/SyncApp.java
@@ -12,6 +12,8 @@ import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 public class SyncApp extends ResourceConfig
 {
 	private static final Logger log = LoggerFactory.getLogger(ResourceConfig.class);
@@ -19,16 +21,26 @@ public class SyncApp extends ResourceConfig
 	@Inject
 	public SyncApp(ServiceLocator locator)
 	{
+		packages("org.nishen.resourcepartners");
+
+		register(io.swagger.jaxrs.listing.ApiListingResource.class);
+		register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 		register(new JaxbMessagingBinder());
 		register(new JaxbParamConverterBinder());
 		register(new MoxyJsonFeature());
-
-		packages("org.nishen.resourcepartners");
 
 		// link guice with hk2
 		GuiceBridge.getGuiceBridge().initializeGuiceBridge(locator);
 		GuiceIntoHK2Bridge guiceBridge = locator.getService(GuiceIntoHK2Bridge.class);
 		guiceBridge.bridgeGuiceInjector(SyncServletContextListener.injector);
+
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setVersion("0.0.8");
+		beanConfig.setSchemes(new String[] { "http", "https" });
+		beanConfig.setBasePath("/partner-sync");
+		beanConfig.setResourcePackage("org.nishen.resourcepartners.resources");
+		beanConfig.setPrettyPrint(true);
+		beanConfig.setScan(true);
 
 		log.debug("instantiated class: {}", this.getClass().getName());
 	}
